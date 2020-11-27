d86af0ecae9ee9180f17f6ff11e7b6c2bde502fb
caitianmi <timocai@hisunglobal.com>
2020-11-27 18:26:09 +0800

    收支明细

--- lib/page_route.dart
+++ lib/page_route.dart
@@ -4,6 +4,8 @@ import 'package:ebank_mobile/page/bankcard/card_limit_manager_page.dart';
 import 'package:ebank_mobile/page/bankcard/card_list_page.dart';
 import 'package:ebank_mobile/page/index_page/hsg_index_page.dart';
 import 'package:ebank_mobile/page/login/login_page.dart';
+import 'package:ebank_mobile/page/payCollectDetail/detail_info_page.dart';
+import 'package:ebank_mobile/page/payCollectDetail/detail_list_page.dart';
 import 'package:flutter/material.dart';
 import 'package:ebank_mobile/page/personalCenter/about_us.dart';
 
@@ -15,6 +17,8 @@ var pageCardDetail = '/card_detail';
 var pageCardLimit = '/card_limit_manager';
 var aboutUs = '/about_us';
 var pageAccountOverview = '/account_overview';
+var pageDetailInfo = '/detail_info_page';
+var pageDetailList = '/detail_list_page';
 var pageIndex = new MaterialPageRoute(builder: (context) => new IndexPage());
 
 var appRoutes = {
@@ -24,6 +28,8 @@ var appRoutes = {
   pageCardLimit: (context) => CardLimitManagerPage(),
   aboutUs: (context) => FirstScreen(),
   pageAccountOverview: (context) => AccountOverviewPage(),
+  pageDetailList: (context) => DetailListPage(),
+  pageDetailInfo: (context) => DetailInfoPage(),
 };
 
 onGenerateRoute(RouteSettings settings) {
