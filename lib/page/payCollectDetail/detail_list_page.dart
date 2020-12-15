24cc0ec4e7c73e2080a8ce009fc7ae94d5c6c647
CaiTM <timocai@hisunglobal.com>
2020-12-15 18:12:25 +0800

    收支明细

--- lib/page/payCollectDetail/detail_list_page.dart
+++ lib/page/payCollectDetail/detail_list_page.dart
@@ -4,12 +4,16 @@
 /// Date: 2020-12-07
 
 import 'package:ebank_mobile/config/hsg_colors.dart';
+import 'package:ebank_mobile/data/source/card_data_repository.dart';
 import 'package:ebank_mobile/data/source/model/get_pay_collect_detail.dart';
 import 'package:ebank_mobile/data/source/pay_collect_detail_repository.dart';
 import 'package:ebank_mobile/generated/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_cupertino_date_picker/flutter_cupertino_date_picker.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:date_format/date_format.dart';
+import 'package:intl/intl.dart';
 
 import '../../page_route.dart';
 
@@ -19,20 +23,24 @@ class DetailListPage extends StatefulWidget {
 }
 
 class _DetailListPageState extends State<DetailListPage> {
+  DateTime _nowDate = DateTime.now(); //当前日期
   List<RevenueHistoryDTOList> revenueHistoryList = [];
+  List<String> cards = [];
+  String startDate = DateFormat('yyyy-MM-' + '01').format(DateTime.now());
 
   @override
   // ignore: must_call_super
   void initState() {
     // 网络请求
-    _getRevenueByCards();
+    _getCardList();
   }
 
   @override
   Widget build(BuildContext context) {
+    var _date = formatDate(_nowDate, [yyyy, '-', mm]);
     return Scaffold(
       appBar: AppBar(
-        title: Text(S.current.transaction_details),
+        title: Text('收支明细'),
         centerTitle: true,
         // elevation: 0,
       ),
@@ -45,21 +53,48 @@ class _DetailListPageState extends State<DetailListPage> {
               color: Colors.white,
               padding: EdgeInsets.only(left: 15, right: 15),
               margin: EdgeInsets.only(bottom: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('2020-12-01'),
-                  Text('全部账户'),
-                ],
-              ),
+              child: Row(children: <Widget>[
+                InkWell(
+                  onTap: _cupertinoPicker,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(_date),
+                      Icon(Icons.arrow_drop_down)
+                    ],
+                  ),
+                ),
+              ]),
             ),
             Expanded(
-              child: CustomScrollView(
-                slivers: _sliversSection(revenueHistoryList),
-              ),
+              child: revenueHistoryList.length > 0
+                  ? CustomScrollView(
+                      slivers: _sliversSection(revenueHistoryList),
+                    )
+                  : _noData(),
             )
           ],
         ),
+      ),
+    );
+  }
+
+  Container _noData() {
+    return Container(
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image(
+            image: AssetImage('images/noDataIcon/no_data_record.png'),
+            width: 160,
+          ),
+          Padding(padding: EdgeInsets.only(top: 25)),
+          Text(
+            '暂无交易记录',
+            style: TextStyle(fontSize: 15, color: HsgColors.secondDegreeText),
+          )
+        ],
       ),
     );
   }
@@ -81,31 +116,28 @@ class _DetailListPageState extends State<DetailListPage> {
         ),
       );
 
-      section.add(
-        SliverList(
-          delegate:
-              SliverChildBuilderDelegate((BuildContext context, int index) {
-            return Column(
-              children: [
-                GestureDetector(
-                  child: Container(
-                    padding: EdgeInsets.fromLTRB(15, 23, 15, 20),
-                    color: Colors.white,
-                    child: _getRow(ddFinHistList, index),
-                  ),
-                  onTap: () {
-                    _goToDetail(ddFinHistList[index]);
-                  },
+      section.add(SliverList(
+        delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
+          return Column(
+            children: [
+              GestureDetector(
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(15, 23, 15, 20),
+                  color: Colors.white,
+                  child: _getRow(ddFinHistList, index),
                 ),
-                Container(
-                  padding: EdgeInsets.only(left: 70, right: 13),
-                  child: Divider(height: 0.5, color: HsgColors.divider),
-                ),
-              ],
-            );
-          }, childCount: listData.ddFinHistDOList.length),
-        ),
-      );
+                onTap: () {
+                  _goToDetail(ddFinHistList[index]);
+                },
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 70, right: 13),
+                child: Divider(height: 0.5, color: HsgColors.divider),
+              ),
+            ],
+          );
+        }, childCount: listData.ddFinHistDOList.length),
+      ));
 
       section.add(
         SliverToBoxAdapter(
@@ -190,16 +222,57 @@ class _DetailListPageState extends State<DetailListPage> {
     Navigator.pushNamed(context, pageDetailInfo, arguments: ddFinHist);
   }
 
-  _getRevenueByCards() async {
+  //调起flutter_cupertino_date_picker选择器
+  _cupertinoPicker() {
+    DatePicker.showDatePicker(
+      context,
+      pickerTheme: DateTimePickerTheme(
+        showTitle: true,
+        // title: Text('选择日期'),
+        confirm: Text('确定', style: TextStyle(color: Colors.black)),
+        cancel: Text('取消', style: TextStyle(color: Colors.black)),
+      ),
+      minDateTime: DateTime.parse('1900-01-01'),
+      maxDateTime: DateTime.now(),
+      initialDateTime: _nowDate,
+      dateFormat: 'yyyy年-MM月',
+      locale: DateTimePickerLocale.zh_cn,
+      onConfirm: (dateTime, List<int> index) {
+        //确定的时候
+        setState(() {
+          startDate = DateFormat('yyyy-MM-' + '01').format(dateTime);
+          _nowDate = dateTime;
+          _getRevenueByCards(startDate);
+        });
+      },
+    );
+  }
+
+  _getRevenueByCards(String localDateStart) async {
     PayCollectDetailRepository()
         .getRevenueByCards(
-            GetRevenueByCardsReq(
-                localDateStart: '2020-11-01', cards: ['500000674001']),
+            GetRevenueByCardsReq(localDateStart: startDate, cards: cards),
             'GetRevenueByCardsReq')
         .then((data) {
       if (data.revenueHistoryDTOList != null) {
         setState(() {
           revenueHistoryList = data.revenueHistoryDTOList;
+        });
+      }
+    }).catchError((e) {
+      Fluttertoast.showToast(msg: e.toString());
+    });
+  }
+
+  _getCardList() async {
+    CardDataRepository().getCardList('getCardList').then((data) {
+      if (data.cardList != null) {
+        setState(() {
+          cards.clear();
+          data.cardList.forEach((item) {
+            cards.add(item.cardNo);
+          });
+          _getRevenueByCards(startDate);
         });
       }
     }).catchError((e) {
