296484006ff9b56f5a4197df9a66bd2b0bb325c3
hlx <473589137@qq.com>
2020-12-29 18:17:19 +0800

    登录页面误提交

--- lib/page/login/login_page.dart
+++ lib/page/login/login_page.dart
@@ -34,13 +34,13 @@ class _LoginPageState extends State<LoginPage> {
   var _changeLangBtnTltle = 'English'; // S.current.english;
 
   final TextEditingController _accountTC =
-      TextEditingController(text: 'huanglixia');
+      TextEditingController(text: 'Smile04');
 
   final TextEditingController _passwordTC =
-      TextEditingController(text: 'sFE9Y06I');
+      TextEditingController(text: 'Qwe123456~');
 
-  var _account = 'huanglixia'; //'18033412021';
-  var _password = 'sFE9Y06I'; //'123456';
+  var _account = 'Smile04'; //'18033412021';
+  var _password = 'Qwe123456~'; //'123456';
 
   @override
   void initState() {
