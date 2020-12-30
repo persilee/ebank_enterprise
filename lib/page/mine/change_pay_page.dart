f953c3727edfb5646001b90995edd2910c8705e3
hlx <473589137@qq.com>
2020-12-30 19:53:45 +0800

    支付密码重置的功能

--- lib/page/mine/change_pay_page.dart
+++ lib/page/mine/change_pay_page.dart
@@ -1,10 +1,20 @@
+import 'dart:async';
+
+import 'package:ebank_mobile/data/source/mine_pay_pwdApi.dart';
+import 'package:ebank_mobile/data/source/model/get_verification_code.dart';
+import 'package:ebank_mobile/data/source/model/set_payment_pwd.dart';
+import 'package:ebank_mobile/data/source/verification_code_repository.dart';
 /**
   @desc   修改支付密码
   @author hlx
  */
 import 'package:ebank_mobile/generated/l10n.dart';
+import 'package:ebank_mobile/util/small_data_store.dart';
+import 'package:ebank_mobile/widget/progressHUD.dart';
 import 'package:flutter/material.dart';
 import 'package:ebank_mobile/config/hsg_colors.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ChangePayPage extends StatefulWidget {
   @override
@@ -19,7 +29,10 @@ class _ChangePayPageState extends State<ChangePayPage> {
   TextEditingController _newPwd = TextEditingController();
   TextEditingController _confimPwd = TextEditingController();
   TextEditingController _sms = TextEditingController();
-
+  
+  Timer _timer;
+  int countdownTime = 0;
+  
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -66,6 +79,23 @@ class _ChangePayPageState extends State<ChangePayPage> {
                             color: HsgColors.divider,
                             indent: 3,
                             endIndent: 3),
+                            
+                        Container(
+                          height: 50,
+                          child: Row(
+                            children: [
+                              Text(S.of(context).sendmsm),
+                              Expanded(
+                                child: otpTextField(),
+                              ),
+                              SizedBox(
+                                width: 90,
+                                height: 32,
+                                child: _otpButton(),
+                              )
+                            ],
+                          ),
+                        ),
                       ],
                     ),
                   ),
@@ -75,7 +105,11 @@ class _ChangePayPageState extends State<ChangePayPage> {
                     width: MediaQuery.of(context).size.width,
                     child: RaisedButton(
                       child: Text(S.of(context).sumit),
-                      onPressed: () => print("提交"),
+                      onPressed: _submit()
+                          ? () {
+                              _submitData();
+                            }
+                          : null,
                       color: HsgColors.accent,
                       textColor: Colors.white,
                       shape: RoundedRectangleBorder(
@@ -87,7 +121,119 @@ class _ChangePayPageState extends State<ChangePayPage> {
               )),
         ));
   }
+
+  //提交按钮
+  _submitData() async {
+    if (_newPwd.text != _confimPwd.text) {
+      Fluttertoast.showToast(msg: S.of(context).differentPwd);
+    } else {
+       HSProgressHUD.show();
+         final prefs = await SharedPreferences.getInstance();
+      String userID = prefs.getString(ConfigKey.USER_ID);
+      PaymentPwdRepository()
+          .updateTransPassword(
+        SetPaymentPwdReq(_oldPwd.text, _newPwd.text, userID, _sms.text),
+        'updateTransPassword',
+      ).then((data) {
+        HSProgressHUD.showError(status: '密码修改成功');
+        Navigator.pop(context);
+        HSProgressHUD.dismiss();
+      }).catchError((e) {
+        // Fluttertoast.showToast(msg: e.toString());
+        HSProgressHUD.showError(status: e.toString());
+        print('${e.toString()}');
+      });
+    }
+  }
+    bool _submit() {
+    if (_oldPwd.text != '' &&
+        _newPwd.text != '' &&
+        _confimPwd.text != '') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  //倒计时方法
+  _startCountdown() {
+    countdownTime = 60;
+    final call = (timer) {
+      setState(() {
+        if (countdownTime < 1) {
+          _timer.cancel();
+        } else {
+          countdownTime -= 1;
+        }
+      });
+    };
+    _timer = Timer.periodic(Duration(seconds: 1), call);
+  }
+
+  //获取验证码按钮
+  OutlineButton _otpButton() {
+    return OutlineButton(
+      onPressed: countdownTime > 0
+          ? null
+          : () {
+              _getVerificationCode();
+            },
+      //为什么要设置左右padding，因为如果不设置，那么会挤压文字空间
+      padding: EdgeInsets.symmetric(horizontal: 8),
+      //文字颜色
+      textColor: HsgColors.blueTextColor,
+      borderSide: BorderSide(color: HsgColors.blueTextColor, width: 0.5),
+      //画圆角
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(4),
+      ),
+      disabledTextColor: HsgColors.describeText,
+      disabledBorderColor: HsgColors.describeText,
+      child: Text(
+        countdownTime > 0 ? '${countdownTime}s' : '获取验证码',
+        style: TextStyle(fontSize: 14),
+      ),
+      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+    );
+  }
+
+  //获取验证码接口
+  _getVerificationCode() async {
+    HSProgressHUD.show();
+    final prefs = await SharedPreferences.getInstance();
+    String userAcc = prefs.getString(ConfigKey.USER_ACCOUNT);
+    VerificationCodeRepository()
+        .sendSmsByAccount(
+            SendSmsByAccountReq('modifyPwd', userAcc), 'SendSmsByAccountReq')
+        .then((data) {
+      _startCountdown();
+      setState(() {
+        _sms.text = '123456';
+      });
+      HSProgressHUD.dismiss();
+    }).catchError((e) {
+      Fluttertoast.showToast(msg: e.toString());
+      HSProgressHUD.dismiss();
+    });
+  }
+
+  //验证码输入框
+  TextField otpTextField() {
+    return TextField(
+      textAlign: TextAlign.end,
+      keyboardType: TextInputType.number,
+      controller: _sms,
+      decoration: InputDecoration.collapsed(
+        hintText: S.current.please_input,
+        hintStyle: TextStyle(
+          fontSize: 14,
+          color: HsgColors.textHintColor,
+        ),
+      ),
+    );
+  }
 }
+
 
 //封装一行
 class InputList extends StatelessWidget {
@@ -133,33 +279,6 @@ class InputList extends StatelessWidget {
               ),
             ),
           ),
-          if (this.isShow)
-            SizedBox(
-              width: 90,
-              height: 32,
-              child: FlatButton(
-                onPressed: () {},
-                //为什么要设置左右padding，因为如果不设置，那么会挤压文字空间
-                padding: EdgeInsets.symmetric(horizontal: 8),
-                //文字颜色
-                textColor: Colors.black12,
-                //按钮颜色
-                color: HsgColors.blueBGColor,
-                //画圆角
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(4),
-                ),
-                //如果使用FlatButton，必须初始化onPressed这个方法
-                // onPressed: () {
-                //   widget.onPressed(_controller.text);
-                // },
-                child: Text(
-                  '获取验证码',
-                  style: TextStyle(fontSize: 14,color: HsgColors.blueTextColor),
-                ),
-                materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-              ),
-            ),
         ],
       ),
     );
