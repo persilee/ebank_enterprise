6824e01d7924e37d870453aceecba0f27ff5d3ef
kendriczhang <kendriczhang@hisunglobal.com>
2020-12-30 19:58:30 +0800

    解决冲突

--- lib/page/approval/authorization_task_approval_page.dart
+++ lib/page/approval/authorization_task_approval_page.dart
@@ -26,20 +26,6 @@ class _AuthorizationTaskApprovalPageState
   Rows history;
   _AuthorizationTaskApprovalPageState(this.history);
   var commentList = <CommentList>[];
-  //转账信息
-  bool _transfer = false;
-  var _fromAccount = "";
-  var _fromCcy = "";
-  var _payeeName = "";
-  //付款信息
-  bool _pay = false;
-  var _accountNumber = "";
-  var _accountName = "";
-  var _payBank = "";
-  var _toCcy = "";
-  var _toaccount = "";
-  var _remark = "";
-
   @override
   void initState() {
     super.initState();
@@ -55,8 +41,53 @@ class _AuthorizationTaskApprovalPageState
       ),
       body: CustomScrollView(
         slivers: <Widget>[
-          _tansferInfo(),
-          _payInfo(),
+          SliverToBoxAdapter(
+            child: Container(
+              color: Colors.white,
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+              padding: EdgeInsets.fromLTRB(15, 10, 15, 0),
+              child: Column(
+                children: [
+                  _getHintLine(),
+                  _getRow('账号名称', 'mike'),
+                  _getHintLine(),
+                  _getRow('转入货币', 'EUR')
+                ],
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+              color: Colors.white,
+              child: Column(
+                children: [
+                  Container(
+                      color: Colors.white,
+                      child: Row(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                            child: Text(
+                              '付款信息',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          )
+                        ],
+                      )),
+                  _getHintLine(),
+                  _getRow('付款账户', history.processId),
+                  _getHintLine(),
+                  _getRow('付款账户', '84981891898'),
+                  _getHintLine(),
+                  _getRow('付款账户', '84981891898'),
+                ],
+              ),
+            ),
+          ),
           SliverToBoxAdapter(
             child: Container(
               color: Colors.white,
@@ -120,94 +151,6 @@ class _AuthorizationTaskApprovalPageState
     );
   }
 
-//转账信息
-  _tansferInfo() {
-    return SliverToBoxAdapter(
-      child: !_transfer
-          ? Container()
-          : Container(
-              padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
-              color: Colors.white,
-              child: Column(
-                children: [
-                  Container(
-                      color: Colors.white,
-                      child: Row(
-                        children: [
-                          Container(
-                            padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                            child: Text(
-                              '转账信息',
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          )
-                        ],
-                      )),
-                  _getHintLine(),
-                  _fromAccount == ""
-                      ? Container()
-                      : _getRow("收款账户", _fromAccount),
-                  _getHintLine(),
-                  _payeeName == "" ? Container() : _getRow("账户名称", _payeeName),
-                  _getHintLine(),
-                  _fromCcy == "" ? Container() : _getRow("转入货币", _fromCcy),
-                ],
-              ),
-            ),
-    );
-  }
-
-//付款信息
-  _payInfo() {
-    return SliverToBoxAdapter(
-      child: !_pay
-          ? Container()
-          : Container(
-              padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
-              color: Colors.white,
-              child: Column(
-                children: [
-                  Container(
-                      color: Colors.white,
-                      child: Row(
-                        children: [
-                          Container(
-                            padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                            child: Text(
-                              '付款信息',
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          )
-                        ],
-                      )),
-                  _getHintLine(),
-                  _accountNumber == ""
-                      ? Container()
-                      : _getRow("付款账户", _accountNumber),
-                  _getHintLine(),
-                  _accountName == ""
-                      ? Container()
-                      : _getRow("账户名称", _accountName),
-                  _getHintLine(),
-                  _payBank == "" ? Container() : _getRow("付款银行", _payBank),
-                  _getHintLine(),
-                  _toCcy == "" ? Container() : _getRow("转出货币", _toCcy),
-                  _getHintLine(),
-                  _toaccount == "" ? Container() : _getRow("转出金额", _toaccount),
-                  _getHintLine(),
-                  _remark == "" ? Container() : _getRow("附言", _remark),
-                ],
-              ),
-            ),
-    );
-  }
-
   _getRow(String leftText, String rightText) {
     return Container(
       padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
@@ -244,21 +187,7 @@ class _AuthorizationTaskApprovalPageState
             commentList.clear();
             commentList.addAll(data.commentList);
           }
-          if (data.operateEndValue != null) {
-            _pay = true;
-            _transfer = true;
-            _accountNumber = data.operateEndValue.payerCardNo;
-            _accountName = data.operateEndValue.payerName;
-            _payBank = data.operateEndValue.payerBankCode;
-            _toCcy = data.operateEndValue.debitCurrency;
-            _toaccount = data.operateEndValue.amount;
-            _remark = data.operateEndValue.remark;
-            _fromAccount = data.operateEndValue.payeeBankCode;
-            _fromCcy = data.operateEndValue.debitCurrency;
-            _payeeName = data.operateEndValue.payeeName;
-          }
         });
-        print(data.commentList.toString());
       })
     });
   }
