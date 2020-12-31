db7efd4dd93158888191cc5da7e6d22b92c10ac6
fangluyao <florencefang@hisunglobal.com>
2020-12-31 16:27:53 +0800

    任务审批国际化

--- lib/page/approval/application_task_approval_page.dart
+++ lib/page/approval/application_task_approval_page.dart
@@ -48,7 +48,7 @@ class _ApplicationTaskApprovalPageState
     print(processId);
     return Scaffold(
       appBar: AppBar(
-        title: Text('任务审批'),
+        title: Text(S.current.task_approval),
         centerTitle: true,
       ),
       body: CustomScrollView(
@@ -64,7 +64,7 @@ class _ApplicationTaskApprovalPageState
                 children: [
                   Container(
                     child: Text(
-                      '审批历史',
+                      S.current.approval_histroy,
                       style: TextStyle(
                         fontSize: 16,
                         fontWeight: FontWeight.bold,
@@ -88,15 +88,17 @@ class _ApplicationTaskApprovalPageState
                               child: Column(
                                 children: [
                                   _getHintLine(),
-                                  //发起人
-                                  _getRow('审批人', commentList[index].userName),
-                                  //待办任务名称
-                                  _getRow('审批时间', commentList[index].time),
-                                  //创建时间
-                                  _getRow('审批意见', commentList[index].comment),
-                                  //审批结果
-                                  _getRow('审批结果',
-                                      commentList[index].result.toString()),
+                                  _getRow(S.current.approver,
+                                      commentList[index].userName),
+                                  _getRow(S.current.approver_time,
+                                      commentList[index].time),
+                                  _getRow(S.current.approver_opinion,
+                                      commentList[index].comment),
+                                  _getRow(
+                                      S.current.approver_result,
+                                      commentList[index].result
+                                          ? S.current.success
+                                          : S.current.failed),
                                 ],
                               ))
                         ],
@@ -109,7 +111,7 @@ class _ApplicationTaskApprovalPageState
                   child: Container(
                     color: Colors.white,
                     padding: EdgeInsets.fromLTRB(15, 5, 15, 15),
-                    child: Text('无内容'),
+                    child: Text(S.current.no_content),
                   ),
                 ),
         ],
@@ -135,7 +137,7 @@ class _ApplicationTaskApprovalPageState
                           Container(
                             padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
                             child: Text(
-                              '转账信息',
+                              S.current.transfer_info,
                               style: TextStyle(
                                 fontSize: 16,
                                 fontWeight: FontWeight.bold,
@@ -147,15 +149,15 @@ class _ApplicationTaskApprovalPageState
                   _getHintLine(),
                   (_fromAccount == "" || _fromAccount == null)
                       ? Container()
-                      : _getRow("收款账户", _fromAccount),
+                      : _getRow(S.current.transfer_to_account, _fromAccount),
                   _getHintLine(),
                   (_payeeName == "" || _fromAccount == null)
                       ? Container()
-                      : _getRow("账户名称", _payeeName),
+                      : _getRow(S.current.userName, _payeeName),
                   _getHintLine(),
                   (_fromCcy == "" || _fromAccount == null)
                       ? Container()
-                      : _getRow("转入货币", _fromCcy),
+                      : _getRow(S.current.from_ccy, _fromCcy),
                 ],
               ),
             ),
@@ -179,7 +181,7 @@ class _ApplicationTaskApprovalPageState
                           Container(
                             padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
                             child: Text(
-                              '付款信息',
+                              S.current.payment_info,
                               style: TextStyle(
                                 fontSize: 16,
                                 fontWeight: FontWeight.bold,
@@ -191,27 +193,27 @@ class _ApplicationTaskApprovalPageState
                   _getHintLine(),
                   (_accountNumber == "" || _fromAccount == null)
                       ? Container()
-                      : _getRow("付款账户", _accountNumber),
+                      : _getRow(S.current.payment_account, _accountNumber),
                   _getHintLine(),
                   (_accountName == "" || _fromAccount == null)
                       ? Container()
-                      : _getRow("账户名称", _accountName),
+                      : _getRow(S.current.userName, _accountName),
                   _getHintLine(),
                   (_payBank == "" || _fromAccount == null)
                       ? Container()
-                      : _getRow("付款银行", _payBank),
+                      : _getRow(S.current.payment_bank, _payBank),
                   _getHintLine(),
                   (_toCcy == "" || _fromAccount == null)
                       ? Container()
-                      : _getRow("转出货币", _toCcy),
+                      : _getRow(S.current.to_ccy, _toCcy),
                   _getHintLine(),
                   (_toaccount == "" || _fromAccount == null)
                       ? Container()
-                      : _getRow("转出金额", _toaccount),
+                      : _getRow(S.current.to_amount, _toaccount),
                   _getHintLine(),
                   (_remark == "" || _fromAccount == null)
                       ? Container()
-                      : _getRow("附言", _remark),
+                      : _getRow(S.current.postscript, _remark),
                 ],
               ),
             ),
--- lib/page/approval/authorization_task_approval_page.dart
+++ lib/page/approval/authorization_task_approval_page.dart
@@ -50,7 +50,7 @@ class _AuthorizationTaskApprovalPageState
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('任务审批'),
+        title: Text(S.current.task_approval),
         centerTitle: true,
       ),
       body: CustomScrollView(
@@ -66,7 +66,7 @@ class _AuthorizationTaskApprovalPageState
                 children: [
                   Container(
                     child: Text(
-                      '审批历史',
+                      S.current.approval_histroy,
                       style: TextStyle(
                         fontSize: 16,
                         fontWeight: FontWeight.bold,
@@ -90,15 +90,17 @@ class _AuthorizationTaskApprovalPageState
                               child: Column(
                                 children: [
                                   _getHintLine(),
-                                  //发起人
-                                  _getRow('审批人', commentList[index].userName),
-                                  //待办任务名称
-                                  _getRow('审批时间', commentList[index].time),
-                                  //创建时间
-                                  _getRow('审批意见', commentList[index].comment),
-                                  //审批结果
-                                  _getRow('审批结果',
-                                      commentList[index].result.toString()),
+                                  _getRow(S.current.approver,
+                                      commentList[index].userName),
+                                  _getRow(S.current.approver_time,
+                                      commentList[index].time),
+                                  _getRow(S.current.approver_opinion,
+                                      commentList[index].comment),
+                                  _getRow(
+                                      S.current.approver_result,
+                                      commentList[index].result
+                                          ? S.current.success
+                                          : S.current.failed),
                                 ],
                               ))
                         ],
@@ -111,7 +113,7 @@ class _AuthorizationTaskApprovalPageState
                   child: Container(
                     color: Colors.white,
                     padding: EdgeInsets.fromLTRB(15, 5, 15, 15),
-                    child: Text('无内容'),
+                    child: Text(S.current.no_content),
                   ),
                 ),
         ],
@@ -137,7 +139,7 @@ class _AuthorizationTaskApprovalPageState
                           Container(
                             padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
                             child: Text(
-                              '转账信息',
+                              S.current.transfer_info,
                               style: TextStyle(
                                 fontSize: 16,
                                 fontWeight: FontWeight.bold,
@@ -149,15 +151,15 @@ class _AuthorizationTaskApprovalPageState
                   _getHintLine(),
                   (_fromAccount == "" || _fromAccount == null)
                       ? Container()
-                      : _getRow("收款账户", _fromAccount),
+                      : _getRow(S.current.transfer_to_account, _fromAccount),
                   _getHintLine(),
                   (_payeeName == "" || _fromAccount == null)
                       ? Container()
-                      : _getRow("账户名称", _payeeName),
+                      : _getRow(S.current.userName, _payeeName),
                   _getHintLine(),
                   (_fromCcy == "" || _fromAccount == null)
                       ? Container()
-                      : _getRow("转入货币", _fromCcy),
+                      : _getRow(S.current.from_ccy, _fromCcy),
                 ],
               ),
             ),
@@ -181,7 +183,7 @@ class _AuthorizationTaskApprovalPageState
                           Container(
                             padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
                             child: Text(
-                              '付款信息',
+                              S.current.payment_info,
                               style: TextStyle(
                                 fontSize: 16,
                                 fontWeight: FontWeight.bold,
@@ -193,27 +195,27 @@ class _AuthorizationTaskApprovalPageState
                   _getHintLine(),
                   (_accountNumber == "" || _fromAccount == null)
                       ? Container()
-                      : _getRow("付款账户", _accountNumber),
+                      : _getRow(S.current.payment_account, _accountNumber),
                   _getHintLine(),
                   (_accountName == "" || _fromAccount == null)
                       ? Container()
-                      : _getRow("账户名称", _accountName),
+                      : _getRow(S.current.userName, _accountName),
                   _getHintLine(),
                   (_payBank == "" || _fromAccount == null)
                       ? Container()
-                      : _getRow("付款银行", _payBank),
+                      : _getRow(S.current.payment_bank, _payBank),
                   _getHintLine(),
                   (_toCcy == "" || _fromAccount == null)
                       ? Container()
-                      : _getRow("转出货币", _toCcy),
+                      : _getRow(S.current.to_ccy, _toCcy),
                   _getHintLine(),
                   (_toaccount == "" || _fromAccount == null)
                       ? Container()
-                      : _getRow("转出金额", _toaccount),
+                      : _getRow(S.current.to_amount, _toaccount),
                   _getHintLine(),
                   (_remark == "" || _fromAccount == null)
                       ? Container()
-                      : _getRow("附言", _remark),
+                      : _getRow(S.current.postscript, _remark),
                 ],
               ),
             ),
