6a6d423ed6beee8a9d6415115d4066f712407ec5
CaiTM <timocai@hisunglobal.com>
2021-01-06 11:26:03 +0800

    添加获取本币接口

--- lib/data/source/public_parameters_repository.dart
+++ lib/data/source/public_parameters_repository.dart
@@ -2,14 +2,22 @@
 /// 获取公共参数
 /// Author: CaiTM
 /// Date: 2020-12-23
+
 import 'package:ebank_mobile/data/source/model/get_public_parameters.dart';
 import 'package:ebank_mobile/http/hsg_http.dart';
 
 class PublicParametersRepository {
-  Future<GetPublicParametersResp> getPublicCode(
-      GetPublicParametersReq req, String tag) {
+  //获取证件类型
+  Future<GetIdTypeResp> getIdType(GetIdTypeReq req, String tag) {
     return request('/platform/publicCode/getPublicCodeByType', req, tag,
-        (data) => GetPublicParametersResp.fromJson(data));
+        (data) => GetIdTypeResp.fromJson(data));
+  }
+
+  //获取本币
+  Future<GetLocalCurrencyResp> getLocalCurrency(
+      GetLocalCurrencyReq req, String tag) {
+    return request('/platform/publicCode/getPublicCodeByType', req, tag,
+        (data) => GetLocalCurrencyResp.fromJson(data));
   }
 
   static final _instance = PublicParametersRepository._internal();
