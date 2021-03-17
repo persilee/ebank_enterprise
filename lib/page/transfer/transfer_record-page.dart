476063f69198d112379d0e70fa371b0f01216694
fangluyao <florencefang@hisunglobal.com>
2021-03-17 15:03:46 +0800

    转账记录增加金额筛选

--- lib/page/transfer/transfer_record-page.dart
+++ lib/page/transfer/transfer_record-page.dart
@@ -15,15 +15,15 @@ import 'package:ebank_mobile/data/source/user_data_repository.dart';
 import 'package:ebank_mobile/generated/l10n.dart' as intl;
 import 'package:ebank_mobile/util/format_util.dart';
 import 'package:ebank_mobile/util/small_data_store.dart';
-import 'package:ebank_mobile/widget/hsg_button.dart';
+import 'package:ebank_mobile/widget/custom_pop_window_button.dart';
 import 'package:ebank_mobile/widget/hsg_dialog.dart';
 import 'package:ebank_mobile/widget/hsg_dotted_line.dart';
+import 'package:ebank_mobile/widget/hsg_general_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_cupertino_date_picker/flutter_cupertino_date_picker.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:intl/intl.dart';
-import 'package:popup_window/popup_window.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../../page_route.dart';
 
@@ -265,6 +265,11 @@ class _TrsnsferRecordPageState extends State<TrsnsferRecordPage> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
+          //时间
+          Container(
+            width: MediaQuery.of(context).size.width / 2.5,
+            child: _popDialog(),
+          ),
           //银行卡
           GestureDetector(
             onTap: _chooseBankCard,
@@ -272,11 +277,6 @@ class _TrsnsferRecordPageState extends State<TrsnsferRecordPage> {
               width: MediaQuery.of(context).size.width / 2,
               child: _headerText(_card),
             ),
-          ),
-          //时间
-          Container(
-            width: MediaQuery.of(context).size.width / 2.5,
-            child: _popDialog(),
           ),
         ],
       ),
@@ -299,8 +299,9 @@ class _TrsnsferRecordPageState extends State<TrsnsferRecordPage> {
 
   //顶部弹窗
   Widget _popDialog() {
-    return PopupWindowButton(
-      offset: Offset(MediaQuery.of(context).size.width / 2.3, 200),
+    return CustomPopupWindowButton(
+      offset: Offset(0, 200),
+      isRelative: true,
       buttonBuilder: (BuildContext context) {
         return GestureDetector(
           child: _headerText(_time),
@@ -323,7 +324,7 @@ class _TrsnsferRecordPageState extends State<TrsnsferRecordPage> {
   Widget _popDialogContent(BuildContext popcontext) {
     return Container(
       color: Colors.white,
-      height: 180,
+      height: 260,
       padding: EdgeInsets.all(10),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -334,6 +335,9 @@ class _TrsnsferRecordPageState extends State<TrsnsferRecordPage> {
           //自定义时间
           _timeText(intl.S.of(context).user_defined),
           _userDefind(popcontext),
+          //金额
+          _timeText('金额'),
+          _amountDuration(),
         ],
       ),
     );
@@ -370,6 +374,73 @@ class _TrsnsferRecordPageState extends State<TrsnsferRecordPage> {
         //确定按钮
         _confimrButton(),
       ],
+    );
+  }
+
+  //金额
+  Widget _amountDuration() {
+    return Row(
+      children: [
+        _amountInput(),
+        Text(
+          intl.S.of(context).zhi,
+          style: TextStyle(
+            fontSize: 12,
+            color: HsgColors.aboutusTextCon,
+            decoration: TextDecoration.none,
+          ),
+        ),
+        _amountInput(),
+        _amountConfimrButton(),
+      ],
+    );
+  }
+
+  //金额输入框
+  Widget _amountInput() {
+    return Container(
+      margin: EdgeInsets.all(5),
+      width: 111.5,
+      height: 23.5,
+      decoration: BoxDecoration(
+        color: Color(0xffECECEC),
+        borderRadius: BorderRadius.circular(5),
+      ),
+      // child:
+      // TextField(
+      //   // controller: controller,
+      //   autocorrect: false,
+      //   autofocus: false,
+      //   keyboardType: TextInputType.number,
+      //   onChanged: (text) {},
+      // ),
+    );
+  }
+
+  //金额确定按钮
+  Widget _amountConfimrButton() {
+    return Container(
+      margin: EdgeInsets.all(4),
+      width: 73,
+      height: 23.5,
+      decoration: BoxDecoration(
+        color: HsgColors.blueTextColor,
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: OutlineButton(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+        borderSide: BorderSide(color: Colors.white),
+        child: Text(
+          intl.S.of(context).confirm,
+          style: TextStyle(fontSize: 11, color: Colors.white),
+        ),
+        onPressed: () {
+          setState(() {
+            _transferHistoryList.clear();
+          });
+          Navigator.of(context).pop(_loadData());
+        },
+      ),
     );
   }
 
