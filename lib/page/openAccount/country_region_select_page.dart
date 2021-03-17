ff7cd19e278bf892033d894e94431e1955a97bf8
lijiawei <815656754@qq.com>
2021-03-17 17:21:35 +0800

    国家地区选择临时页面

---
+++ lib/page/openAccount/country_region_select_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CountryOrRegionSelectPage extends StatefulWidget {
+  @override
+  _CountryOrRegionSelectPageState createState() =>
+      _CountryOrRegionSelectPageState();
+}
+
+class _CountryOrRegionSelectPageState extends State<CountryOrRegionSelectPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
