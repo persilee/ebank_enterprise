aea54912b0c85df659bbea08d4db26f509e6d65b
kendriczhang <kendriczhang@hisunglobal.com>
2021-03-20 09:38:10 +0800

    溢出调整

--- lib/page/login/login_page.dart
+++ lib/page/login/login_page.dart
@@ -117,57 +117,47 @@ class _LoginPageState extends State<LoginPage> {
         ),
         Container(
           child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              Expanded(
-                flex: 1,
-                child: Container(),
-              ),
               //忘记用户名
-              Expanded(
-                flex: 2,
-                child: Container(
-                  height: 20,
-                  margin: EdgeInsets.only(top: 10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Container(
-                        margin: EdgeInsets.only(left: 15),
-                        child: ForgetButton('忘记用户名', () {
-                          setState(() {
-                            Navigator.pushNamed(context, pageForgetUserName);
-                            print('忘记密码');
-                          });
-                        }),
-                      )
-                    ],
-                  ),
+              Container(
+                height: 20,
+                margin: EdgeInsets.only(top: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(left: 15),
+                      child: ForgetButton('忘记用户名', () {
+                        setState(() {
+                          Navigator.pushNamed(context, pageForgetUserName);
+                          print('忘记密码');
+                        });
+                      }),
+                    )
+                  ],
                 ),
               ),
               //忘记密码按钮
-              Expanded(
-                flex: 2,
-                child: Container(
-                  height: 20,
-                  margin: EdgeInsets.only(top: 10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Container(
-                        padding: EdgeInsets.only(right: 33),
-                        margin: EdgeInsets.only(left: 15),
-                        child:
-                            ForgetButton(S.of(context).fotget_password_q, () {
-                          setState(() {
-                            Navigator.pushNamed(context, pageForgetPassword);
-                            print('忘记密码');
-                          });
-                        }),
-                      )
-                    ],
-                  ),
+              Container(
+                height: 20,
+                margin: EdgeInsets.only(top: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.only(right: 33),
+                      margin: EdgeInsets.only(left: 15),
+                      child: ForgetButton(S.of(context).fotget_password_q, () {
+                        setState(() {
+                          Navigator.pushNamed(context, pageForgetPassword);
+                          print('忘记密码');
+                        });
+                      }),
+                    )
+                  ],
                 ),
-              )
+              ),
             ],
           ),
         ),
