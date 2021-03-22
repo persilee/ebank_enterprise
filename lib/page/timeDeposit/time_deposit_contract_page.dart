9f6182f0d167e280c0f5b44e110fed91df60964f
wangluyao <coralinewang@hisunglobal.com>
2021-03-22 21:04:43 +0800

    修改定期页面

--- lib/page/timeDeposit/time_deposit_contract_page.dart
+++ lib/page/timeDeposit/time_deposit_contract_page.dart
@@ -4,7 +4,9 @@
 /// Date: 2020-12-14
 import 'package:ebank_mobile/config/hsg_colors.dart';
 import 'package:ebank_mobile/data/source/card_data_repository.dart';
+import 'package:ebank_mobile/data/source/model/forex_trading.dart';
 import 'package:ebank_mobile/data/source/model/get_card_list.dart';
+import 'package:ebank_mobile/data/source/model/get_single_card_bal.dart';
 import 'package:ebank_mobile/data/source/model/time_deposit_contract.dart';
 import 'package:ebank_mobile/data/source/model/time_deposit_contract_trial.dart';
 import 'package:ebank_mobile/data/source/model/time_deposit_product.dart';
@@ -39,6 +41,7 @@ class _TimeDepositContractState extends State<TimeDepositContract> {
   String _changedInstructionTitle = S.current.hint_please_select;
   int _acPosition = 0;
   int _settAcPosition = 0;
+  int _ccyPosition = 0;
   String _changedRateTitle = '';
   String accuPeriod = '0';
   String auctCale = '0';
@@ -49,6 +52,11 @@ class _TimeDepositContractState extends State<TimeDepositContract> {
   List<RemoteBankCard> cards = [];
   RemoteBankCard card;
   String custID;
+  String _cardCcy = '';
+  String _cardBal = '';
+  List<String> _cardCcyList;
+  List<String> _cardBalList;
+  List<List<CardBalBean>> myCardListBal;
 
   double bal = 0.00;
   String instCode = '';
@@ -62,9 +70,10 @@ class _TimeDepositContractState extends State<TimeDepositContract> {
 
   void initState() {
     super.initState();
-    _first();
+
     //网络请求
     _loadData();
+    _first();
   }
 
 //背景色
@@ -371,6 +380,26 @@ class _TimeDepositContractState extends State<TimeDepositContract> {
     );
   }
 
+  //选择支付币种按钮
+  Widget _ccyChangeBtn() {
+    return Container(
+      color: Colors.white,
+      child: FlatButton(
+        onPressed: () {
+          _selectCcy(context);
+        },
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            _leftText('支付币种'),
+            _display(_cardCcy, '支付币种'),
+            _rightArrow(),
+          ],
+        ),
+      ),
+    );
+  }
+
   //选择结算账户按钮
   Widget _settAcChangeBtn() {
     return Container(
@@ -544,10 +573,40 @@ class _TimeDepositContractState extends State<TimeDepositContract> {
     if (result != null && result != false) {
       _acPosition = result;
       _changedAccountTitle = accounts[result];
+      _getCardBal(accounts[result].replaceAll(new RegExp(r"\s+\b|\b\s"), ""));
     } else {
       return;
     }
     setState(() {});
+  }
+
+//支付币种弹窗
+  _selectCcy(BuildContext context) async {
+    List<String> bals = [];
+    List<String> ccys = [];
+    // List<String> ciNames = [];
+    for (List<CardBalBean> cardBalList in myCardListBal) {
+      for (CardBalBean cardBalBean in cardBalList) {
+        ccys.add(cardBalBean.ccy);
+        bals.add(cardBalBean.currBal);
+      }
+    }
+    // for (var i = 0; i < bankCards.length; i++) {
+    //   accounts.add(FormatUtil.formatSpace4(bankCards[i]));
+    // }
+    final result = await showHsgBottomSheet(
+        context: context,
+        builder: (context) => HsgBottomSingleChoice(
+            title: '支付币种', items: ccys, lastSelectedPosition: _ccyPosition));
+    if (result != null && result != false) {
+      _ccyPosition = result;
+      _cardCcy = ccys[result];
+      _cardBal = bals[result];
+    } else {
+      return;
+    }
+    setState(() {});
+    (context as Element).markNeedsBuild();
   }
 
 //结算账户弹窗
@@ -674,6 +733,7 @@ class _TimeDepositContractState extends State<TimeDepositContract> {
           _termChangeBtn(context, producDTOList), // 选择存款期限
           _inputPrincipal(card), // 本金输入框
           _accountChangeBtn(), //选择付款账户
+          _ccyChangeBtn(), //选择支付币种
           _line(),
           _settAcChangeBtn(), //结算账户
           _line(),
@@ -684,7 +744,26 @@ class _TimeDepositContractState extends State<TimeDepositContract> {
     );
   }
 
-//数据初值
+  Future<void> _loadData() async {
+    final prefs = await SharedPreferences.getInstance();
+    custID = prefs.getString(ConfigKey.CUST_ID);
+    CardDataRepository().getCardList('getCardList').then(
+      (data) {
+        if (data.cardList != null) {
+          setState(() {
+            cards.clear();
+            cards.addAll(data.cardList);
+            _changedAccountTitle = cards[0].cardNo;
+            _getCardBal(cards[0].cardNo);
+          });
+        }
+      },
+    ).catchError((e) {
+      Fluttertoast.showToast(msg: e.toString());
+    });
+  }
+
+  //数据初值
   void _first() {
     //按计提周期计算存款期限
     switch (producDTOList[0].minAccuPeriod) {
@@ -714,23 +793,8 @@ class _TimeDepositContractState extends State<TimeDepositContract> {
     depositType = producDTOList[0].depositType;
     ccy = producDTOList[0].ccy;
     rate = FormatUtil.formatNum(double.parse(producDTOList[0].minRate), 2);
-  }
-
-  Future<void> _loadData() async {
-    final prefs = await SharedPreferences.getInstance();
-    custID = prefs.getString(ConfigKey.CUST_ID);
-    CardDataRepository().getCardList('getCardList').then(
-      (data) {
-        if (data.cardList != null) {
-          setState(() {
-            cards.clear();
-            cards.addAll(data.cardList);
-          });
-        }
-      },
-    ).catchError((e) {
-      Fluttertoast.showToast(msg: e.toString());
-    });
+    // _cardCcy = _cardCcyList[0];
+    // _cardBal = _cardBalList[0];
   }
 
   Future<void> _loadDepositData(
@@ -754,6 +818,54 @@ class _TimeDepositContractState extends State<TimeDepositContract> {
     }).catchError((e) {
       Fluttertoast.showToast(msg: e.toString());
     });
+  }
+
+  //获取付款账户余额
+  _getCardBal(String cardNo) {
+    Future.wait({
+      CardDataRepository().getCardBalByCardNo(
+          GetSingleCardBalReq(cardNo), 'GetSingleCardBalReq'),
+    }).then((value) {
+      value.forEach((element) {
+        // setState(() {
+        //       if (_cardBal == '' || _cardCcy == '') {
+        //       _cardBal = element.cardListBal[0].currBal;
+        //       _cardCcy = element.cardListBal[0].ccy;
+        //       element.cardListBal.forEach((element) {
+        //         // if (element.ccy == _localeCcy) {
+        //         //   _payCcy = element.ccy;
+        //         //   _balance = element.currBal;
+        //         // }
+        //       });
+        //     });
+        setState(() {
+          if (_cardBal == '' || _cardCcy == '') {
+            _cardBal = element.cardListBal[0].currBal;
+            _cardCcy = element.cardListBal[0].ccy;
+            element.cardListBal.forEach((element) {
+              // if (element.ccy == _localeCcy) {
+              //   _payCcy = element.ccy;
+              //   _balance = element.currBal;
+              // }
+            });
+          }
+        });
+      });
+
+      // if (value != null) {
+      //   setState(() {
+      //     _cardCcy = value.cardListBal[0].ccy;
+      //     _cardBal = value.cardListBal[0].currBal;
+      //     myCardListBal.clear();
+      //     (value.cardListBal).forEach((data) {
+      //       myCardListBal.add(value.cardListBal);
+      //     });
+
+      //     // _cardCcyList.clear();
+      //     // _cardBalList.clear();
+      //   });
+      // }
+    }).catchError((e) {});
   }
 
   Future<void> _loadContractData(
