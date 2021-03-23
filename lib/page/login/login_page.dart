94815f61f3abd1b34478b0275f58e6cf5d467e4a
magicKen <i@magicken.com>
2021-03-23 11:17:38 +0800

    feat: 修改启动页，避免变形；修改图标名称

--- lib/page/login/login_page.dart
+++ lib/page/login/login_page.dart
@@ -17,7 +17,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../util/encrypt_util.dart';
