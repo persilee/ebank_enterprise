f230a055cd2fd40882663087ffc0b0a4b0c56c38
kendriczhang <kendriczhang@hisunglobal.com>
2021-03-27 16:01:20 +0800

    刷新加载

--- lib/page/login/login_page.dart
+++ lib/page/login/login_page.dart
@@ -38,7 +38,7 @@ class _LoginPageState extends State<LoginPage> {
   var _changeLangBtnTltle = '';
 
   TextEditingController _accountTC =
-      TextEditingController(text: 'blk201'); //fangluyao
+      TextEditingController(text: 'blk501'); //fangluyao
   TextEditingController _passwordTC =
       TextEditingController(text: '4N0021S8'); //b0S25X5Y
   var _account = ''; //'blk101';
