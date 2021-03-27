e6c8d8a5062f4ec3bde547bba9254807764fdf3d
kendriczhang <kendriczhang@hisunglobal.com>
2021-03-27 15:25:12 +0800

    加载组件

--- lib/page/login/login_page.dart
+++ lib/page/login/login_page.dart
@@ -41,8 +41,8 @@ class _LoginPageState extends State<LoginPage> {
       TextEditingController(text: 'blk502'); //fangluyao
   TextEditingController _passwordTC =
       TextEditingController(text: '4N0021S8'); //b0S25X5Y
-  var _account = 'blk502'; //'blk101';
-  var _password = '4N0021S8'; //'4N0021S8';
+  var _account = ''; //'blk101';
+  var _password = ''; //'4N0021S8';
 
   // TextEditingController _accountTC =
   //     TextEditingController(text: 'blk302'); //fangluyao
