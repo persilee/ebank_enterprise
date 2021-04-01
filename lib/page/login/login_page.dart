33e1b3ab3703809ad99953d1f920afa9f0c452c5
persilee <i@lishaoy.net>
2021-04-01 11:48:46 +0800

    修改网络请求模块

--- lib/page/login/login_page.dart
+++ lib/page/login/login_page.dart
@@ -35,11 +35,11 @@ class _LoginPageState extends State<LoginPage> {
   var _changeLangBtnTltle = '';
 
   TextEditingController _accountTC =
-      TextEditingController(text: ''); //fangluyao
+      TextEditingController(text: 'blk603'); //fangluyao
   TextEditingController _passwordTC =
-      TextEditingController(text: 'Aa123456@'); //b0S25X5Y
-  var _account = ''; //'blk101';HSG20
-  var _password = ''; //'4N0021S8';Qwe123456~
+      TextEditingController(text: '4N0021S8'); //b0S25X5Y
+  var _account = 'blk603'; //'blk101';HSG20
+  var _password = '4N0021S8'; //'4N0021S8';Qwe123456~
 
   // TextEditingController _accountTC =
   //     TextEditingController(text: 'blk302'); //HSG20
