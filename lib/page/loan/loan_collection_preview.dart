102ed1cf734f687cf9b0a0220356e4f9ba6d6117
wangluyao <coralinewang@hisunglobal.com>
2021-04-02 15:56:40 +0800

    提交贷款领用预览页面

--- lib/page/loan/loan_collection_preview.dart
+++ lib/page/loan/loan_collection_preview.dart
@@ -11,8 +11,15 @@ class PageLoanCollectionPreview extends StatefulWidget {
 class _PageLoanCollectionPreviewState extends State<PageLoanCollectionPreview> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: _rowText("asd", "123"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("转账预览"),
+        centerTitle: true,
+        elevation: 1,
+      ),
+      body: Container(
+        child: _previewInfo(),
+      ),
     );
   }
 
@@ -31,6 +38,31 @@ class _PageLoanCollectionPreviewState extends State<PageLoanCollectionPreview> {
         children: [
           _textStyle(leftText, Colors.black, 15),
           _textStyle(rightText, Colors.black, 15),
+        ],
+      ),
+    );
+  }
+
+//领用金额
+  Widget _previewHeader() {
+    return Container(
+      padding: EdgeInsets.fromLTRB(16, 42.5, 16, 42.5),
+      child: _rowText("可借款额度", "USD 800"),
+    );
+  }
+
+//预览信息
+  Widget _previewInfo() {
+    return Container(
+      child: Row(
+        children: [
+          _rowText("可借款额度", "USD 800"),
+          _rowText("借款期限", "12个月"),
+          _rowText("还款方式", "按月付息"),
+          _rowText("还款计划", "首期4月28"),
+          _rowText("总利息", "51.4"),
+          _rowText("收款账户", "2444 4547 4545"),
+          _rowText("借款用途", "无"),
         ],
       ),
     );
