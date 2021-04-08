13c902d74e60af410d4d36dc8d41623b066025e7
persilee <i@lishaoy.net>
2021-04-08 14:37:17 +0800

    新增pdf查看器

--- lib/page/approval/my_to_do_task_detail_page.dart
+++ lib/page/approval/my_to_do_task_detail_page.dart
@@ -834,6 +834,7 @@ class _MyToDoTaskDetailPageState extends State<MyToDoTaskDetailPage> {
           return HsgTipsDialog(
             child: Text(S.current.approve_reject_tip),
             confirmCallback: () {
+              FocusManager.instance.primaryFocus?.unfocus();
               Navigator.pop(context);
               _rejectTaskConfirm(context);
             },
@@ -880,6 +881,7 @@ class _MyToDoTaskDetailPageState extends State<MyToDoTaskDetailPage> {
           return HsgTipsDialog(
             child: Text(S.current.approve_reject_to_start_tip),
             confirmCallback: () {
+              FocusManager.instance.primaryFocus?.unfocus();
               Navigator.pop(context);
               _rejectToStartTaskConfirm();
             },
@@ -942,6 +944,7 @@ class _MyToDoTaskDetailPageState extends State<MyToDoTaskDetailPage> {
     bool isPassword =  await _openBottomSheet();
     // 如果交易密码正确，处理审批逻辑
     if(isPassword) {
+      FocusManager.instance.primaryFocus?.unfocus();
       if (this.mounted) {
         setState(() {
           _btnIsLoadingEAA = true;
