d57b30d412c6f0ecccd2a214130907e336b57ec3
fangluyao <florencefang@hisunglobal.com>
2021-04-15 20:30:20 +0800

    修改行内转账和跨行转账

--- lib/data/source/model/get_transfer_by_account.dart
+++ lib/data/source/model/get_transfer_by_account.dart
@@ -76,48 +76,48 @@ class GetTransferByAccount extends Object {
 
 @JsonSerializable()
 class TransferByAccountResp extends Object {
-  @JsonKey(name: 'payerBankCode')
-  String payerBankCode;
+  // @JsonKey(name: 'payerBankCode')
+  // String payerBankCode;
 
-  @JsonKey(name: 'payerCardNo')
-  String payerCardNo;
+  // @JsonKey(name: 'payerCardNo')
+  // String payerCardNo;
 
-  @JsonKey(name: 'payeeBankCode')
-  String payeeBankCode;
+  // @JsonKey(name: 'payeeBankCode')
+  // String payeeBankCode;
 
-  @JsonKey(name: 'payeeCardNo')
-  String payeeCardNo;
+  // @JsonKey(name: 'payeeCardNo')
+  // String payeeCardNo;
 
-  @JsonKey(name: 'payeeName')
-  String payeeName;
+  // @JsonKey(name: 'payeeName')
+  // String payeeName;
 
-  @JsonKey(name: 'amount')
-  String amount;
+  // @JsonKey(name: 'amount')
+  // String amount;
 
-  @JsonKey(name: 'remark')
-  String remark;
+  // @JsonKey(name: 'remark')
+  // String remark;
 
-  @JsonKey(name: 'debitCurrency')
-  String debitCurrency;
+  // @JsonKey(name: 'debitCurrency')
+  // String debitCurrency;
 
-  @JsonKey(name: 'creditCurrency')
-  String creditCurrency;
+  // @JsonKey(name: 'creditCurrency')
+  // String creditCurrency;
 
-  @JsonKey(name: 'xRate')
-  String xRate;
+  // @JsonKey(name: 'xRate')
+  // String xRate;
 
   TransferByAccountResp(
-    this.payerBankCode,
-    this.payerCardNo,
-    this.payeeBankCode,
-    this.payeeCardNo,
-    this.payeeName,
-    this.amount,
-    this.remark,
-    this.debitCurrency,
-    this.creditCurrency,
-    this.xRate,
-  );
+      // this.payerBankCode,
+      // this.payerCardNo,
+      // this.payeeBankCode,
+      // this.payeeCardNo,
+      // this.payeeName,
+      // this.amount,
+      // this.remark,
+      // this.debitCurrency,
+      // this.creditCurrency,
+      // this.xRate,
+      );
 
   factory TransferByAccountResp.fromJson(Map<String, dynamic> srcJson) =>
       _$TransferByAccountRespFromJson(srcJson);
--- lib/page/transfer/hsg_transfer_internal_preview_page.dart
+++ lib/page/transfer/hsg_transfer_internal_preview_page.dart
@@ -178,7 +178,12 @@ class _TransferInternalPreviewPageState
     String remark = transferData.transferRemark;
     String smsCode = '';
     String xRate = transferData.xRate;
-    print("付款：" + debitAmount + "收款：" + creditAmount);
+    print("付款：" +
+        debitAmount +
+        "收款：" +
+        creditAmount +
+        "payerBankCode: " +
+        payerBankCode);
     HSProgressHUD.show();
     TransferDataRepository()
         .getTransferByAccount(
@@ -189,9 +194,9 @@ class _TransferInternalPreviewPageState
               //收款金额
               creditAmount,
               //贷方货币
-              transferOutCcy,
-              //借方货币
               transferIntoCcy,
+              //借方货币
+              transferOutCcy,
               //输入密码
               // 'L5o+WYWLFVSCqHbd0Szu4Q==',
               '',
@@ -216,6 +221,7 @@ class _TransferInternalPreviewPageState
             'getTransferByAccount')
         .then((data) {
       HSProgressHUD.dismiss();
+      print("==================跳转");
       Navigator.pushReplacementNamed(context, pageOperationResult);
     }).catchError((e) {
       HSProgressHUD.dismiss();
