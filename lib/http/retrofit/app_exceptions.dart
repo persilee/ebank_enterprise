cdd47f2de6035e03299b12db4f21b3045eccab47
persilee <i@lishaoy.net>
2021-04-28 15:29:01 +0800

    修改统一跳转登录页

--- lib/http/retrofit/app_exceptions.dart
+++ lib/http/retrofit/app_exceptions.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:ebank_mobile/main.dart';
+import 'package:ebank_mobile/page/login/login_page.dart';
 import 'package:ebank_mobile/widget/hsg_error_page.dart';
 import 'package:flutter/material.dart';
 
@@ -108,12 +109,17 @@ class AppException implements Exception {
         {
           if (error.error.code == 'SYS90018' ||
               error.error.code == 'SYS90017') {
-            // navigatorKey.currentState.push(MaterialPageRoute(
-            //   builder: (context) => HsgErrorPage(
-            //     title: error.error.code,
-            //     desc: error.error.message,
-            //   ),
-            // ));
+            navigatorKey.currentState.pushAndRemoveUntil(
+                MaterialPageRoute(builder: (BuildContext context) {
+                  return LoginPage();
+                }), (Route route) {
+              //一直关闭，直到首页时停止，停止时，整个应用只有首页和当前页面
+              print(route.settings?.name);
+              if (route.settings?.name == "/") {
+                return true; //停止关闭
+              }
+              return false; //继续关闭
+            });
             return error.error = NeedLogin();
           }
           if (error.error is SocketException) {
