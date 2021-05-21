1b2821b20e01ea497bc971129c2c9438e3ee294c
lijiawei <815656754@qq.com>
2021-05-21 18:21:29 +0800

    修改获取输入的加密后的交易密码

--- lib/util/pay_password_check.dart
+++ lib/util/pay_password_check.dart
@@ -8,12 +8,12 @@ import 'package:flutter/widgets.dart';
 import 'package:sp_util/sp_util.dart';
 
 // ignore: non_constant_identifier_names
-Future<bool> CheckPayPassword(BuildContext context, VoidCallback callback) {
+Future<String> CheckPayPassword(BuildContext context, VoidCallback callback) {
   return _openBottomSheet(context, callback);
 }
 
 //点击提校验交易密码
-Future<bool> _openBottomSheet(
+Future<String> _openBottomSheet(
     BuildContext context, VoidCallback callback) async {
   bool passwordEnabled = SpUtil.getBool(ConfigKey.USER_PASSWORDENABLED);
   // 判断是否设置交易密码，如果没有设置，跳转到设置密码页面，
@@ -27,31 +27,36 @@ Future<bool> _openBottomSheet(
         }
       },
     );
-    return false;
+    return '';
   } else {
     // 输入交易密码
-    bool isPassword = await _didBottomSheet(context);
+    String password = await _didBottomSheet(context);
     // 如果交易密码正确，处理审批逻辑
-    if (isPassword) {
+    if (password.length > 0) {
       callback();
     }
-    return isPassword;
+    return password;
   }
 }
 
 //交易密码窗口
-Future<bool> _didBottomSheet(BuildContext context) async {
+Future<String> _didBottomSheet(BuildContext context) async {
+  String passwordStr = '';
   final isPassword = await showHsgBottomSheet(
       context: context,
       builder: (context) {
         return HsgPasswordDialog(
           title: S.current.input_password,
           isDialog: false,
+          returnPasswordFunc: (password) {
+            print('>>>>>>>>>>>>>>>>>>>>>> $password');
+            passwordStr = password ?? '';
+          },
         );
       });
   if (isPassword != null && isPassword == true) {
-    return true;
+    return passwordStr;
   }
   FocusManager.instance.primaryFocus?.unfocus();
-  return false;
+  return '';
 }
